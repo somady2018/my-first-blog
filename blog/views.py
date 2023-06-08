8309c4fa3cba33b1ec1df78c955a678eae055091
somady2018 <129972299+somady2018@users.noreply.github.com>
2023-06-08 14:23:50 +0900

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,16 @@ from .forms import PostForm
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
 
+
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
