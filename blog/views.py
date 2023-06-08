b9fbabb5e21efa27d56fbd356d2033c3309e1aae
somady2018 <129972299+somady2018@users.noreply.github.com>
2023-06-08 14:51:52 +0900

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from .forms import PostForm
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_new(request):
     if request.method == "POST":
