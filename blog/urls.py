19ae93853f29d4baddaf514cb7d919932c31e1ad
lmj <lmj8922@gmail.com>
2023-06-11 07:25:07 +0900

    login logout delete

--- blog/urls.py
+++ blog/urls.py
@@ -7,15 +7,3 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
-
-from django.conf.urls import include, url
-from django.contrib import admin
-
-from django.contrib.auth import views
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
-    url(r'', include('blog.urls')),
-]
